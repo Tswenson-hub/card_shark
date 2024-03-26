bb7575f42f56c1fb33bc5954a4fc83a7c4f9f336
Tyler Swenson <57569000+Tswenson-hub@users.noreply.github.com>
2024-03-26 16:07:37 -0500

    Add files via upload

--- Upload_Card.py
+++ Upload_Card.py
@@ -1,11 +1,16 @@
 import streamlit as st
 from ximilar.client import CollectiblesRecognitionClient
+import json
 
 
 capture = st.camera_input('Upload card photo')
 pic = [{'_url': capture}]
 #pic = [{'_url': 'https://images.ximilar.com/examples/cards/jordan.jpeg'}]
 
-if pic != None:
-    recog_client = CollectiblesRecognitionClient(token="e972a5215c18316047d2113cb13d8462a036c601")
-    print(recog_client.process(pic))
+recog_client = CollectiblesRecognitionClient(token="e972a5215c18316047d2113cb13d8462a036c601")
+result = recog_client.process(pic)
+
+if result['status']['code'] != 500:
+    j = json.dumps(result, indent=4, sort_keys=True)
+    st.write(j)
+
